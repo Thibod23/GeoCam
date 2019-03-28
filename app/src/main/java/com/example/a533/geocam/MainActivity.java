035b712e84e7f8096ec28af91c829fdf1b34302f
Anthony Thibodeau <antho_exoskate@hotmail.com>
2019-03-28 14:27:52 -0400

    Essaie de shits pour la camera

--- app/src/main/java/com/example/a533/geocam/MainActivity.java
+++ app/src/main/java/com/example/a533/geocam/MainActivity.java
@@ -1,13 +1,98 @@
 package com.example.a533.geocam;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.nfc.Tag;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.os.Environment.getExternalStorageDirectory;
+import static android.os.Environment.getExternalStoragePublicDirectory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "Main_Activity";
+
+    Button takePictureButton;
+    String currentPhotoPath;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setListener();
+    }
+
+    private void setListener(){
+        takePictureButton = (Button)findViewById(R.id.btnTakePicture);
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    private void dispatchTakePictureIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null){
+            //Create the File where the photo should go
+            File photoFile = null;
+            try{
+                photoFile = createImageFile();
+            }catch (IOException ex){
+                Log.d("TAG",ex.getMessage());
+            }
+            // Continue only if the File was successfully created
+            Log.d(TAG, "avant if");
+            if(photoFile != null){
+                Log.d(TAG, "dans if");
+                Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                galleryAddPic();
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException{
+        //Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM) + "/Camera/");
+        if(!storageDir.exists()){
+            storageDir.mkdirs();
+        }
+        File image = File.createTempFile(
+                imageFileName, /*prefix*/
+                ".jpg", /*suffix*/
+                storageDir    /*directory*/
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
     }
 }
