80ef8a20b90ef86af9f152ce6804f1592c6ab8be
B533P-02\533 <test@hotmail.com>
2019-04-29 09:00:05 -0400

    on retourne les photos dans une list

--- app/src/main/java/com/example/a533/geocam/AfficherPhotosActivity.java
+++ app/src/main/java/com/example/a533/geocam/AfficherPhotosActivity.java
@@ -1,24 +1,24 @@
 package com.example.a533.geocam;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.widget.Toast;
+
+import com.example.a533.geocam.model.Picture;
+import com.example.a533.geocam.repository.PictureRepository;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class AfficherPhotosActivity extends AppCompatActivity {
 
     private ArrayList<String> mImageNames = new ArrayList<>();
     private ArrayList<Bitmap> mImages = new ArrayList<>();
     private ArrayList<String> mPositions = new ArrayList<>();
+    private PictureRepository pictureRepository = new PictureRepository();
 
 
     @Override
@@ -30,23 +30,22 @@ public class AfficherPhotosActivity extends AppCompatActivity {
     }
 
     private void initTest(){
+        PictureRepository.MyCallbackFillListPictures todoWhenFinished = new PictureRepository.MyCallbackFillListPictures() {
+            @Override
+            public void onQueryFinished(List<Picture> pictures) {
+                fillRecyclerViewData(pictures);
+            }
+        };
+        pictureRepository.getAll(todoWhenFinished);
 
+    }
 
-        mImages.add(BitmapFactory.decodeFile("/storage/emulated/0/Android/data/com.example.a533.geocam/files/Pictures/test1_20190411_182639_7283655670496709015.jpg"));
-        mImages.add(BitmapFactory.decodeFile("/storage/emulated/0/Android/data/com.example.a533.geocam/files/Pictures/test2_20190411_182622_159972999237839762.jpg" ));
-        mImages.add(BitmapFactory.decodeFile("/storage/emulated/0/Android/data/com.example.a533.geocam/files/Pictures/test3_20190411_182628_7934302385539901012.jpg"));
-        mImages.add(BitmapFactory.decodeFile("/storage/emulated/0/Android/data/com.example.a533.geocam/files/Pictures/test4_20190411_182633_7183693964757802967.jpg"));
-
-        mImageNames.add("Test 1");
-        mImageNames.add("Test 2");
-        mImageNames.add("Test 3");
-        mImageNames.add("Test 4");
-
-        mPositions.add("1312 642");
-        mPositions.add("5456 686");
-        mPositions.add("6795 153");
-        mPositions.add("5626 844");
-
+    private void fillRecyclerViewData(List<Picture> pictures){
+        for (Picture picture: pictures) {
+            mImages.add(BitmapFactory.decodeFile("/storage/emulated/0/Android/data/com.example.a533.geocam/files/Pictures/" + picture.name));
+            mImageNames.add(picture.name.split("_")[0]);
+            mPositions.add(String.format("%s, %s", picture.lat, picture.lng));
+        }
         initRecyclerView();
     }
 
--- app/src/main/java/com/example/a533/geocam/repository/PictureRepository.java
+++ app/src/main/java/com/example/a533/geocam/repository/PictureRepository.java
@@ -11,7 +11,10 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PictureRepository {
@@ -22,8 +25,32 @@ public class PictureRepository {
         this.database = FirebaseFirestore.getInstance();
     }
 
-    public List<Picture> getAll() {
-        throw new Error("Not implemented");
+    public interface MyCallbackFillListPictures{
+        void onQueryFinished(List<Picture> pictures);
+    }
+
+    public void getAll(final MyCallbackFillListPictures todoWhenFinished) {
+
+        database.collection("Pictures")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            List<Picture> pictures = new ArrayList<>();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                pictures.add(new Picture(document.get("name").toString(), document.get("lat").toString(), document.get("lng").toString()));
+                            }
+                            //fillList(pictures, PictureRepository.MyCallbackFillListPictures);
+                            todoWhenFinished.onQueryFinished(pictures);
+                        }
+                    }
+                });
+
+    }
+
+    public void fillList(final List<Picture> pictures, final PictureRepository.MyCallbackFillListPictures myCallback){
+
     }
 
     public void findByName(String name) {
